2917f68261dfa76eab892e3fc3fb0b73c0c56f9c
cmillsy <cmillsy@users.noreply.github.com>
2024-01-26 15:22:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # this is a test200
-from ota import OTAUpdater
-from WIFI_CONFIG import SSID, PASSWORD
+#from ota import OTAUpdater
+#from WIFI_CONFIG import SSID, PASSWORD
 from machine import Pin, Timer
 
-firmware_url = "https://raw.githubusercontent.com/cmillsy/chessboard1/main/"
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-ota_updater.download_and_install_update_if_available()
+#firmware_url = "https://raw.githubusercontent.com/cmillsy/chessboard1/main/"
+#ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+#ota_updater.download_and_install_update_if_available()
 
 led = Pin("LED", Pin.OUT)
 timer = Timer()
