b45f50f595c7e1b59f981112a431d4483a8e770d
cmillsy <cmillsy@users.noreply.github.com>
2024-01-26 13:53:59 +0000

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # this is a test
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+firmware_url = "https://raw.githubusercontent.com/cmillsy/chessboard1/tree"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+ota_updater.download_and_install_update_if_available()
+
+from machine import Pin, Timer
+led = Pin("LED", Pin.OUT)
+timer = Timer()
+
+def blink(timer):
+    led.toggle()
+
+timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
